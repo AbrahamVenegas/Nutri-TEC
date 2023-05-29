d07dc3c9a257cba0859ac7cb5dd5e01b7f70794d
JanM <jmarschatz@gmail.com>
2023-05-28 19:06:56 -0600

    Se inicia la ventana de Register

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { LoginNutri } from "./pages/LoginNutri";
 import { LoginCliente } from "./pages/LoginCliente";
+import { RegisterNutri } from "./pages/RegistroNutri";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginNutri />} /> {/* LoginNutri */}
           <Route path="/LoginCliente" element={<LoginCliente />} />
+          <Route path="/RegisterNutri" element={<RegisterNutri />} />
         </Routes>
       </Router>
     </>

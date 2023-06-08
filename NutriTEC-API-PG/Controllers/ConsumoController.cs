adaa29d13bf34a43adb0d470d463acb5d7c84ace
JanM <jmarschatz@gmail.com>
2023-06-07 19:22:49 -0600

    Se edita el controller Consumo

--- NutriTEC-API-PG/Controllers/ConsumoController.cs
+++ NutriTEC-API-PG/Controllers/ConsumoController.cs
@@ -17,77 +17,30 @@ namespace NutriTEC_API_PG.Controllers
 
         // GET: Se muestran los datos obtenidos 
         [HttpGet("Get")]
-        public async Task<ActionResult<List<Cliente>>> Get()
+        public async Task<ActionResult<List<Consumo>>> Get()
         {
-            return Ok(await _context.Clientes.ToListAsync());
+            return Ok(await _context.Consumos.ToListAsync());
         }
 
         // GET by id: Se muestran los datos obtenidos por ID 
         [HttpGet("Get_Id_Cliente")]
-        public async Task<ActionResult<List<Cliente>>> Get(int id_cliente)
+        public async Task<ActionResult<List<Consumo>>> Get(int id_cliente)
         {
-            var dbCliente = await _context.Clientes.FindAsync(id_cliente);
-            if (dbCliente == null)
+            var dbConsumo = await _context.Consumos.FindAsync(id_cliente);
+            if (dbConsumo == null)
             {
                 return BadRequest("Cliente no encontrado");
             }
-            return Ok(dbCliente);
+            return Ok(dbConsumo);
         }
 
         // POST: Se guardan los datos
         [HttpPost("Post")]
-        public async Task<ActionResult<List<Cliente>>> Post(Cliente cliente)
+        public async Task<ActionResult<List<Consumo>>> Post(Consumo cliente)
         {
-            _context.Clientes.Add(cliente);
+            _context.Consumos.Add(cliente);
             await _context.SaveChangesAsync();
-            return Ok(await _context.Clientes.ToListAsync());
-        }
-
-        // PUT: Se actualiza los datos
-        [HttpPut("Edit")]
-        public async Task<ActionResult<List<Cliente>>> Put(Cliente request)
-        {
-            var dbCliente = await _context.Clientes.FindAsync(request.Id);
-            if (dbCliente == null)
-            {
-                return BadRequest("Cliente no encontrado");
-            }
-
-            dbCliente.Nombre = request.Nombre;
-            dbCliente.Apellido1 = request.Apellido1;
-            dbCliente.Apellido2 = request.Apellido2;
-            dbCliente.Edad = request.Edad;
-            dbCliente.FechaNacimiento = request.FechaNacimiento;
-            dbCliente.Peso = request.Peso;
-            dbCliente.Altura = request.Altura;
-            dbCliente.Imc = request.Imc;
-            dbCliente.PaisResidencia = request.PaisResidencia;
-            dbCliente.CaloriasMaximas = request.CaloriasMaximas;
-            dbCliente.Email = request.Email;
-            dbCliente.Password = request.Password;
-            dbCliente.NombreUsuario = request.NombreUsuario;
-            dbCliente.Medidas = request.Medidas;
-            dbCliente.Direccion = request.Direccion;
-
-
-            await _context.SaveChangesAsync();
-
-            return Ok(await _context.Clientes.ToListAsync());
-        }
-
-        // DELETE: se elimina un dato
-        [HttpDelete("Delete")]
-        public async Task<ActionResult<List<Cliente>>> Delete(int Id)
-        {
-            var dbCliente = await _context.Clientes.FindAsync(Id);
-            if (dbCliente == null)
-            {
-                return BadRequest("Cliente no encontrado");
-            }
-            _context.Clientes.Remove(dbCliente);
-            await _context.SaveChangesAsync();
-
-            return Ok(await _context.Clientes.ToListAsync());
+            return Ok(await _context.Consumos.ToListAsync());
         }
 
     }
